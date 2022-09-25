b87b5e53535c47a1f2a58255270533cb862614f5
hafidh2001 <hafidhfauzan2@gmail.com>
2022-09-25 14:57:08 +0700

    create function migrations

---
+++ migrations/dataTable.js
@@ -0,0 +1,24 @@
+const dataTable = [];
+
+// query
+const create_admins = `CREATE TABLE IF NOT EXISTS admins
+(
+    admin_id serial PRIMARY KEY,
+    email    VARCHAR ( 255 ) UNIQUE NOT NULL,
+    password VARCHAR ( 255 ) NOT NULL,
+    created_date timestamp NOT NULL,
+    modified_date timestamp NOT NULL
+);`;
+dataTable.push(create_admins);
+
+const create_customers = `CREATE TABLE IF NOT EXISTS customers
+(
+    customer_id serial PRIMARY KEY,
+    email    VARCHAR ( 255 ) UNIQUE NOT NULL,
+    password VARCHAR ( 255 ) NOT NULL,
+    created_date timestamp NOT NULL,
+    modified_date timestamp NOT NULL
+);`;
+dataTable.push(create_customers);
+
+export default dataTable;
